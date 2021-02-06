de202f1a92b0f294ce26ab1582eda224769747b5
Ricardo Monterrosa <rickyantonio117@gmail.com>
2021-02-06 11:09:21 -0800

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $(function() {
+    // Pause carousel
     $(".carousel").carousel( {interval: 2000, pause: "false" });
+
+    // toggle carousel button
     $("#carouselButton").click(function(){
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
@@ -10,5 +13,15 @@ $(function() {
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause");
         }
+    });
+
+    // show login modal
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('show');
+    });
+
+    // show reserve modal
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('show');
     });
 });
